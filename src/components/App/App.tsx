9a1f9f51249d14773aa59a7f5f627c81bf75847c
RomanRiyanov <yezgo123@gmail.com>
2023-06-07 19:48:23 +0600

    [develop] start developing

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <h2>Title</h2>
+      <p>start</p>
+    </>
+  )
+}
+
+export default App
